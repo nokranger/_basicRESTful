d801f5300fcfb359bc9649e48de26253d3133940
nokranger <anusorn.thavornporn@gmail.com>
2019-03-21 02:16:10 +0700

    login JWT signin

--- api/route/user.js
+++ api/route/user.js
@@ -2,8 +2,30 @@ const express = require('express')
 const route = express.Router()
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt =require('jsonwebtoken')
 
 const User = require('../models/user')
+
+route.get('/alluser',(req,res,next)=>{
+    User.find()
+    .select('_id password email')
+    .exec()
+    .then(user =>{
+        let allusers = {
+            count : user.length,
+            usermail : user.map(users =>{
+                return {
+                    _id : users._id,
+                    pass : users.password,
+                    mail : users.email
+                }
+
+            })
+
+        }
+        res.status(200).json(allusers)
+    })
+})
 
 route.post('/signup', (req, res, next) => {
     User.find({email : req.body.email})
@@ -44,21 +66,47 @@ route.post('/signup', (req, res, next) => {
     })
 })
 
-// route.delete('/:userId',(res,res,next) => {
-//     User.deleteOne({_id : req.params._id})
-//     .exec()
-//     .then(res => {
-//         res.status(200).json({
-//             message : 'User deleted'
-//         })
-//     })
-//     .catch(err => {
-//         console.log(err)
-//         res.status(500).json({
-//             error : err
-//         })
-//     })
-// })
+route.post('/login',(req,res,next) => {
+    User.find({ email : req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length < 1){
+            return res.status(404).json({
+                message : 'Auth failed'
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password,(err,result) => {
+            if(err){
+                return res.status(401).json({
+                    message : 'Auth failed'
+                })
+            }
+            if(result){
+                const token = jwt.sign({
+                    email : user[0].email,
+                    userId : user[0]._id
+                },process.env.JWT_KEY,{
+                    expiresIn : "1h"
+                })
+                return res.status(200).json({
+                    message : 'Auth success',
+                    token : token
+                })
+            }
+            res.status(401).json({
+                message : 'Auth failed'
+            })
+            
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error : err
+        })
+    })
+})
+
 route.delete('/:userId',(req, res, next) => {
     User.deleteOne({_id : req.params.userId})
     .exec()
